b53763cfa2c638b07342b084c9205ae714ba9553
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-10-29 13:06:31 +0800

    Create backend for list items

--- program3.js
+++ program3.js
@@ -1,7 +1,57 @@
-let selectedItem;
+let selectedItem = null;
 
 const homePage = document.getElementById('home-page');
 const paymentPage = document.getElementById('payment-page');
+
+const items = [
+  {
+    title: `Python Crash Course, 2nd Edition: A Hands-On, Project-Based
+            Introduction to Programming 2nd Edition`,
+    price: '$14.80',
+    source: 'python-crash-course.jpg'
+  },
+  {
+    title: 'HTML and CSS: Design and Build Websites 1st Edition',
+    price: '$13.90',
+    source: 'html-css.jpg'
+  },
+  {
+    title: `JavaScript: The Definitive Guide: Master the World's Most-Used
+            Programming Language 7th Edition`,
+    price: '$20.12',
+    source: 'javascript.jpg'
+  },
+  {
+    title: 'Let the app decide for me',
+    price: '???',
+    source: 'red-random.png'
+  }
+]
+
+const choiceContainer = document.getElementById('choice-container');
+
+for (let i = 0; i < items.length; i++) {
+  const newItem = document.createElement('div');
+
+  newItem.id = `choice${i + 1}`;
+  newItem.className = 'choice';
+  newItem.innerHTML =
+    `<img class="thmb" src="${items[i].source}" alt="" />
+      <div class="item-info">
+        <div class="item-info-top">
+          <h4>${items[i].title}</h4>
+        </div>
+        <div class="item-info-bot">
+          <h3>${items[i].price}</h3>
+          <button class="select-btn" onclick="setSelectedItem(${i + 1})">
+            Select
+          </button>
+        </div>
+      </div>`;
+
+  choiceContainer.append(newItem);
+}
+
 const selectButtons = document.querySelectorAll('.select-btn');
 
 selectButtons.forEach((button) => {
@@ -20,12 +70,12 @@ const showCheckout = () => {
   const checkOutElement = document.createElement('div');
 
   if (foundCheckout) return;
+  if (!selectedItem) return;
 
+  checkOutElement.id = 'checkout';
   checkOutElement.innerHTML =
-    `<div id="checkout">
-      <h3>Proceed to Payment</h3>
-      <button id="checkOutButton" onclick="checkOut()">Checkout</button>
-     </div>`;
+    `<h3>Proceed to Payment</h3>
+    <button id="checkOutButton" onclick="checkOut()">Checkout</button>`;
 
   homePage.append(checkOutElement);
 }
@@ -35,20 +85,41 @@ const checkOut = () => {
   const paymentElement = document.createElement('div');
   const book = document.getElementById(`choice${selectedItem}`);
 
+  if (foundPayment) return;
+
+  paymentElement.id = 'payment';
   paymentElement.innerHTML =
-    `<div id="payment">
-      <div id="payment-header">
-        <h2>Louie's Startup</h2>
+    `<div id="payment-header">
         <img id="banner-logo" src="html-logo.png" />
-        <hr />
+        <h2 id="banner-text">Louie's Startup</h2>
+        <button id="exit-payment-btn" onclick="removePayment()">&#10060;</button>
       </div> 
+        <hr />
       <div id="body">
-        <div id="book-details"></div>
-        ${book.innerHTML}
-      </div>
-    </div>`
+        <div id="book-details">
+          <img id="book-to-buy" src="${items[selectedItem - 1].source}" />
+        </div>
+    </div>`;
 
   paymentPage.append(paymentElement);
+}
+
+const removePayment = () => {
+  const paymentElement = document.getElementById('payment');
+  const checkOutElement = document.getElementById('checkout');
+
+  paymentPage.removeChild(paymentElement);
+  homePage.removeChild(checkOutElement);
+
+  removeSelectedItem();
+}
+
+const removeSelectedItem = () => {
+  selectButtons.forEach((button) => {
+    button.style.backgroundColor = 'white';
+    button.style.color = 'black';
+  })
+  selectedItem = null;
 }
 
 const checkIfPressed = () => {
