93c4213a091b72dc6a279dc7668653f316d22975
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-10-29 16:49:54 +0800

    Create receipt

--- program3.js
+++ program3.js
@@ -90,26 +90,26 @@ const checkOut = () => {
   paymentElement.id = 'payment';
   paymentElement.innerHTML =
     `<div id="payment-header">
-        <img id="banner-logo" src="html-logo.png" />
-        <h2 id="banner-text">Louie's Startup</h2>
-        <button id="exit-payment-btn" onclick="removePayment()">&#10060;</button>
-      </div> 
-        <hr />
-      <div id="payment-body">
-        <div id="book-details">
-          <img id="book-to-buy" src="${book.source}" />
-          <div id="book-to-buy-details">
-            <h1>${book.title}</h1>
-            <h2>$${book.price}</h2>
-          </div>
+      <img id="banner-logo" src="html-logo.png" />
+      <h2 id="banner-text">Louie's Startup</h2>
+      <button id="exit-payment-btn" onclick="removePayment()">&#10060;</button>
+    </div> 
+    <hr />
+    <div id="payment-body">
+      <div id="book-details">
+        <img id="book-to-buy" src="${book.source}" />
+        <div id="book-to-buy-details">
+          <h1>${book.title}</h1>
+          <h2>$${book.price}</h2>
         </div>
-        <div id="payment-method">
-          <div id="money-input">
-            <label id="input-label">Input Money: </label>
-            <input type="number" id="money-input-field" />
-          </div>
-          <button id="submit-money-btn" onclick="processPayment()">Buy</button>
+      </div>
+      <div id="payment-method">
+        <div id="money-input">
+          <label id="input-label">Input Money: </label>
+          <input type="number" id="money-input-field" />
         </div>
+        <button id="submit-money-btn" onclick="processPayment()">Buy</button>
+      </div>
     </div>`;
 
   paymentPage.append(paymentElement);
@@ -129,6 +129,7 @@ const processPayment = () => {
   paymentResult.id = 'payment-result';
 
   if (sufficientBalance) {
+    paymentResult.className = 'success';
 
     paymentResult.innerHTML =
       `<div id="payment-details">
@@ -139,8 +140,10 @@ const processPayment = () => {
         <h2>Amount Due: $${book.price}</h2>
         <h2>Amount Paid: $${moneyPaid}</h2>
         <h2>Change: $${Math.round(100 * (moneyPaid - Number(book.price))) / 100}</h2>
-      </>
-      <button id="receipt-btn">Print Receipt</button>`;
+      </div>
+      <div id="payment-button-container">
+        <button id="receipt-btn" onclick="printReceipt(${moneyPaid})">Print Receipt</button>
+      </div>`;
 
     paymentBody.removeChild(paymentMethod);
   } else {
@@ -153,6 +156,87 @@ const processPayment = () => {
   }
 
   paymentBody.append(paymentResult);
+}
+
+const printReceipt = (moneyPaid) => {
+  const book = items[selectedItem - 1];
+  const printWindow = window.open('', '', 'height=700', 'width=600');
+  const text =
+    `<div id="payment-header">
+      <img id="banner-logo" src="html-logo.png" />
+      <h2 id="banner-text">Louie's Startup</h2>
+    </div> 
+    <div id="payment-result">
+      <div id="payment-details">
+        <div id="payment-result-header">
+          <h1>Payment Received!</h1>
+          <img id="payment-result-logo" src="check.png" />
+        </div>
+        <h2>${book.title}</h2>
+        <h2>Amount Due: $${book.price}</h2>
+        <h2>Amount Paid: $${moneyPaid}</h2>
+        <h2>Change: $${Math.round(100 * (moneyPaid - Number(book.price))) / 100}</h2>
+      </div>
+    </div>
+    
+    <style>
+      #payment-header {
+        display: flex;
+        flex-direction: row;
+        margin-left: 110px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        justify-content: start;
+      }
+
+      #payment-body {
+        padding: 20px;
+        overflow-y: scroll;
+        max-width: 700px;
+      }
+
+      #banner-logo {
+        width: 70px;
+        border-radius: 50%;
+        margin-left: -100px;
+      }
+
+      #banner-text {
+        margin-left: 20px;
+      }
+
+      #payment-result {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-top: 10px;
+        padding: 20px;
+
+        border: 2px;
+        border-style: solid;
+        border-radius: 5px;
+        background-color: aliceblue;
+      }
+
+      #payment-result-header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+
+      #payment-result-logo {
+        width: 40px;
+        margin-left: 10px;
+      }
+
+      .success {
+        flex-direction: row;
+      }
+    </style>`;
+
+  printWindow.document.write(text);
+  printWindow.document.close();
+  printWindow.print();
 }
 
 const removePayment = () => {
